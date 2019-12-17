35de659c827373aece68d7d6205e5a66ab355e16
shraddha <41799663+shraddhasable@users.noreply.github.com>
2019-12-16 22:12:56 -0800

    Update carousal.js

--- src/components/carousal.js
+++ src/components/carousal.js
@@ -17,22 +17,22 @@ class SimpleSlider extends React.Component {
     return (
       <Slider {...settings}>
         <div>
-          <Img src={require('../images/carousel-header_1280x506.jpg')} alt="" />
+          <img src={require('../images/carousel-header_1280x506.jpg')} alt="" />
         </div>
         <div>
-        <Img src={require('../images/small-dogs-live-large.jpg')} alt="" />
+        <img src={require('../images/small-dogs-live-large.jpg')} alt="" />
         </div>
         <div>
-        <Img src={require('../images/little-pouch.jpg')} alt="" />
+        <img src={require('../images/little-pouch.jpg')} alt="" />
         </div>
         <div>
-        <Img src={require('../images/puppy-basketball.jpg')} alt="" />
+        <img src={require('../images/puppy-basketball.jpg')} alt="" />
         </div>
         <div>
-        <Img src={require('../images/feed-the-good-tug-of-war.jpg')} alt="" />
+        <img src={require('../images/feed-the-good-tug-of-war.jpg')} alt="" />
         </div>
         <div>
-        <Img src={require('../images/puppy-bowl-carousel-image_1280x506.jpg')} alt="" />
+        <img src={require('../images/puppy-bowl-carousel-image_1280x506.jpg')} alt="" />
         </div>
       </Slider>
     );
